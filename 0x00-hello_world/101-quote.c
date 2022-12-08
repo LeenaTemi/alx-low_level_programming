3c6a5760344c14638cc3f804c8d12de583a939a6
Helen <helentemitope66@gmail.com>
2022-12-08 15:17:39 -0800

    hgd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: 1
+ * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
