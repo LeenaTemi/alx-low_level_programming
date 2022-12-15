f9548d1d48450d96a44c037226fecfdc8c32fd63
Helen <helentemitope66@gmail.com>
2022-12-15 12:01:38 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,16 +7,13 @@
 int main(void)
 {
 	int i;
-	
+	/*blank space*/
 	printf("Infinite loop incoming :(\n");
-	
 	i = 0;
-	
 	/*while (i < 10)*/
 	/*{*/
-		/*putchar(i);*/	
+	/*putchar(i);*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
